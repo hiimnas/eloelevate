5dc5778e2277174f911e84238230d41466f69130
nas <103116522+hiimnas@users.noreply.github.com>
2023-03-21 13:33:55 +0000

    "eloelevate" upload

---
+++ src/app/boosting/boosting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boosting',
+  templateUrl: './boosting.component.html',
+  styleUrls: ['./boosting.component.css']
+})
+export class BoostingComponent {
+
+}
